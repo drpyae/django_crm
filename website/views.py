27d3af4926852bb316a8a7593e1f532da3c9e2f3
Pyae Aung <drpyaephyoaung@outlook.com>
2023-04-01 10:46:00 +0630

    add boostrap and auth form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 def home(request):
+    # Check to see if loggin in
+    if request.method == 'POST':
+        username = request.POST['user_name']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username =user_name, password = password)
+        if user is not None:
+            login(request, user)
+            message.success(request, "You have succesfully Logged In")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging in, please try again.")
+    else:
+        return render(request, 'home.html',{})
+            
     return render(request, 'home.html',{})
+def login_user(request):
+    pass
+def logout_user(request):
+    pass
